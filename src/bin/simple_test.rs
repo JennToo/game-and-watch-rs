bc2633e9dddc8b6dbd65a0fc7d160d6ceb10daa0
Jennifer Wilcox <jennifer@nitori.org>
2020-12-19 19:18:08 -0600

    Fix the linking issue
    I think one of these crates is using an older version of `cortex-m`,
    which is causing duplicate symbol issues. Downgrading that crate seems
    to fix it.

--- src/bin/simple_test.rs
+++ src/bin/simple_test.rs
@@ -10,15 +10,9 @@ use stm32h7::stm32h7b3;
 
 #[entry]
 fn main() -> ! {
-    let peripherals = stm32h7b3::Peripherals::take().unwrap();
-
-    // TODO: This is just junk to make sure `peripherals` doesn't get optimized out yet
-    let gpioa = &peripherals.GPIOA;
-    gpioa.odr.modify(|_, w| w.odr0().set_bit());
+    let _peripherals = stm32h7b3::Peripherals::take().unwrap();
 
     loop {
-        // TODO: Why is the hprintln causing linking errors???
-        //hprintln!("Hello from Rust!");
-        panic!("Hello from Rust!");
+        hprintln!("Hello from Rust!").unwrap();
     }
 }
